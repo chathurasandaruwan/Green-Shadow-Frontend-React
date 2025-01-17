f9b8b27890ccbdba585576ee5daff65bc99f5793
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-01-17 13:28:46 +0530

    create Table component

---
+++ src/component/Table.tsx
@@ -0,0 +1,22 @@
+export function Table(props:any) {
+    return (
+        <div className={props.firstDivClass}>
+            <div className={props.secondDivClass}>
+                <table id={props.id} className={props.tableClass}>
+                    <thead>
+                    <tr>
+                        {props.headers.map((header:string, index:number) => (
+                            <th key={index} scope="col">
+                                {header}
+                            </th>
+                        ))}
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {props.tbody}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
