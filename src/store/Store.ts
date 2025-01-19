9460b9b13897752af2a21815eaf15f7808b4c298
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-01-19 11:23:04 +0530

    create EquipmentData space in Store using Slice

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 import CropSlice from "../slices/CropSlice.ts";
 import StaffSlice from "../slices/StaffSlice.ts";
 import VehicleSlice from "../slices/VehicleSlice.ts";
+import EquipmentSlice from "../slices/EquipmentSlice.ts";
 
 export const store = configureStore({
     reducer:{
         cropsData : CropSlice,
         staffData : StaffSlice,
-        vehicleData : VehicleSlice
+        vehicleData : VehicleSlice,
+        EquipmentData : EquipmentSlice
     }
 })
