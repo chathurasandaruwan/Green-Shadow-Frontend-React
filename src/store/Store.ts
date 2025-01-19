551545a10b169308240a5ce3407897e405d0bde4
chathurasandaruwan <chathurasandaruwan122@gamail.com>
2025-01-19 12:30:35 +0530

    create field space in Store using FieldSlice

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -3,12 +3,14 @@ import CropSlice from "../slices/CropSlice.ts";
 import StaffSlice from "../slices/StaffSlice.ts";
 import VehicleSlice from "../slices/VehicleSlice.ts";
 import EquipmentSlice from "../slices/EquipmentSlice.ts";
+import FieldSlice from "../slices/FieldSlice.ts";
 
 export const store = configureStore({
     reducer:{
         cropsData : CropSlice,
         staffData : StaffSlice,
         vehicleData : VehicleSlice,
-        EquipmentData : EquipmentSlice
+        EquipmentData : EquipmentSlice,
+        FieldData : FieldSlice
     }
 })
